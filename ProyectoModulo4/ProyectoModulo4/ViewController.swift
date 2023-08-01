7bda84600b22bb303be657eb3de266ff23a4a639
Fer Ugalde <fernando.ugalde.mtz@gmail.com>
2023-07-31 23:10:28 -0500

    Adding items to the View controller

---
+++ ProyectoModulo4/ProyectoModulo4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProyectoModulo4
+//
+//  Created by Fernando Ugalde on 31/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var UserName: UILabel!
+    @IBOutlet weak var UserImage: UIImageView!
+    @IBOutlet weak var UserDescription: UILabel!
+    @IBOutlet weak var UserTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
