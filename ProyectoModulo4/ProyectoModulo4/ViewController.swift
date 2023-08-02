e896167620fc0155623b770977f3649976e54126
Fer Ugalde <fernando.ugalde.mtz@gmail.com>
2023-08-01 23:07:53 -0500

    Se agrego collectionView, se inicio las estructuras para la obtencion y llenado del table view

--- ProyectoModulo4/ProyectoModulo4/ViewController.swift
+++ ProyectoModulo4/ProyectoModulo4/ViewController.swift
@@ -9,16 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var UserName: UILabel!
-    @IBOutlet weak var UserImage: UIImageView!
-    @IBOutlet weak var UserDescription: UILabel!
-    @IBOutlet weak var UserTableView: UITableView!
+    struct DataSourse {
+         func downloadUser() -> User {
+            .init(userName: "FerUgalde",
+                  name: "Fernando Ugalde",
+                  dob: "25/03/1996",
+                  followers: 3)
+        }
+    }
+    struct User {
+        let userName: String
+        let name: String
+        let dob: String
+        let followers: UInt
+    }
+
+  
+    @IBOutlet weak var imagePorfile: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var dob: UILabel!
+    @IBOutlet weak var followers: UILabel!
     
+    func setUpUser(_ user: User?){
+        guard let user else { return }
+        userName.text = user.userName
+        name.text = user.name
+        dob.text = user.dob
+        followers.text = String(user.followers)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        let user = DataSourse.downloadUser()
+    
+        setUpUser(user)
 
 
 }
